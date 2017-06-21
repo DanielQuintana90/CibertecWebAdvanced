abfa3640135d8839952c68ae2da236ea891632c3
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-06-20 22:23:43 -0500

    Mock y fluentassertions

--- Cibertec.Repositories.Test/CustomerRepositoryTests.cs
+++ Cibertec.Repositories.Test/CustomerRepositoryTests.cs
@@ -1,6 +1,8 @@
+using Cibertec.Models;
 using Cibertec.UnitOfWork;
 using System.Linq;
 using Xunit;
+using FluentAssertions;
 
 namespace Cibertec.Repositories.Tests
 {
@@ -10,15 +12,55 @@ namespace Cibertec.Repositories.Tests
 
         public CustomerRepositoryTests()
         {
-            _unit = new CibertecUnitOfWork(ConfigSettings.ConnectionString);
+            _unit = MockedUnitOfWork.GetUnitOfWork();
         }
 
         [Fact(DisplayName = "[CustomerRepositoryTests] Get_All_Customers")]
         public void Get_All_Customers()
         {
             var result = _unit.Customers.GetAll();
+            result.Should().NotBeNull();
+            result.Count().Should().BeGreaterThan(0);
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Get_By_Id_Customer")]
+        public void Get_By_Id()
+        {
+            var result = _unit.Customers.GetById(1);
+            Assert.NotNull(result);            
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Insert_Customer")]
+        public void Insert_Customer()
+        {
+            var result = _unit.Customers.Insert(null);
+            result.Should().BeGreaterThan(0);
+        }
+
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Insert_Customer_Fail")]
+        public void Insert_Customer_Wrong()
+        {
+            var result = _unit.Customers.Insert(new Customer());
+            result.Should().Be(0);
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Update_Customer")]
+        public void Update_Customer()
+        {
+            var result = _unit.Customers.Update(null);
+
             Assert.NotNull(result);
-            Assert.True(result.Count() > 0);
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "[CustomerRepositoryTests] Delete_Customer")]
+        public void Delete_Customer()
+        {
+            var result = _unit.Customers.Delete(null);
+
+            Assert.NotNull(result);
+            Assert.True(result);
         }
     }
 }
---
+++ Cibertec.Repositories.Test/MockedUnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using Cibertec.Models;
+using Cibertec.UnitOfWork;
+using Moq;
+using System.Collections.Generic;
+
+namespace Cibertec.Repositories.Tests
+{
+    public static class MockedUnitOfWork
+    {
+        public static IUnitOfWork GetUnitOfWork()
+        {
+            Mock<IUnitOfWork> unit = new Mock<IUnitOfWork>();
+            unit.ConfigureCustomer();
+            return unit.Object;
+        }
+    }
+
+    public static class MockedUnitOfWorkExtensions
+    {
+        public static Mock<IUnitOfWork> ConfigureCustomer(this Mock<IUnitOfWork> mock)
+        {
+            mock.Setup(c => c.Customers.GetAll()).Returns(new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "Daniel",
+                    LastName = "Quintana",
+                    Phone = "123456789",
+                    Country = "Perú",
+                    City = "Lima",
+                    Orders = new List<Order>()
+                },
+                new Customer
+                {
+                    Id = 1,
+                    FirstName = "André",
+                    LastName = "Bedoya",
+                    Phone = "987654321",
+                    Country = "Brasil",
+                    City = "Sao Paulo",
+                    Orders = new List<Order>()
+                }
+            });
+
+            mock.Setup(c => c.Customers.GetById(1)).Returns(new Customer
+            {
+                Id = 1,
+                FirstName = "Daniel",
+                LastName = "Quintana",
+                Phone = "123456789",
+                Country = "Perú",
+                City = "Lima",
+                Orders = new List<Order>()
+            });
+
+            mock.Setup(c => c.Customers.Insert(null)).Returns(1);
+
+            mock.Setup(c => c.Customers.Update(null)).Returns(true);
+
+            mock.Setup(c => c.Customers.Delete(null)).Returns(true);
+
+            return mock;
+        }
+    }
+}
