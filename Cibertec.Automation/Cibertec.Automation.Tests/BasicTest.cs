8752ddc93e6ea282882b6e91187555335c1806b5
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-06-22 22:24:27 -0500

    Proyecto con selenium

---
+++ Cibertec.Automation/Cibertec.Automation.Tests/BasicTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Cibertec.Automation.Tests
+{
+    public class BasicTest
+    {
+        [Fact]
+        public void Navigate_To_Google()
+        {
+            SimpleTest.Navigate();
+        }
+    }
+}
