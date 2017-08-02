1928fa81355971c68f410f8dc3f2bb2f3925a832
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-08-01 22:18:21 -0500

    Web Worker

--- Cibertec.Angular/wwwroot/app/private/csv-viewer/csv.controller.js
+++ Cibertec.Angular/wwwroot/app/private/csv-viewer/csv.controller.js
@@ -6,22 +6,51 @@
     function loginController(configService, $state) {
         var vm = this;
 
+        vm.primaryList = [];
+
+        vm.processFile = processFile;
+
         var fileInput = document.getElementById("csvViewer");
 
         init();
 
         function init() {
             if (!configService.getLogin()) $state.go('login');
-            fileInput.addEventListener('change', readFile);
         }
 
-        function readFile() {
+        function processFile() {
+            
+            vm.primaryList = [];
+
+            readFile(function (result) {
+                var list = [];
+                var totalLines = result.split('\r\n').length - 1;
+                var count = 0;
+                var csvWorker = new Worker("/js/worker.js");
+
+                csvWorker.addEventListener('message', function (message) {
+                    list.push(message.data);
+                    console.log('Processing...');
+                    count++;
+                    if (count >= totalLines) csvWorker.terminate();
+                });
+                               
+                csvWorker.postMessage(result);                         
+
+            });
+
+        }
+
+        function readFile(callback) {
             var reader = new FileReader();
+
             reader.onload = function () {
-                document.getElementById('content').innerHTML = reader.result;
+                return callback(reader.result);
             };
+
             reader.readAsBinaryString(fileInput.files[0]);
         }
+
     }
 
 })();
---
+++ Cibertec.Angular/wwwroot/js/worker.js
@@ -0,0 +1,34 @@
+﻿var self = this;
+
+self.onmessage = function (doc) {
+    
+    var lines = doc.data.split('\r\n');
+
+    for (var i = 1; i < lines.length; i++) {
+
+        var line = lines[i].split(',');
+
+        var member = {};
+
+        member.member_no = line[0];
+        member.lastname = line[1];
+        member.firstname = line[2];
+        member.middleinitial = line[3];
+        member.street = line[4];
+        member.city = line[5];
+        member.state_prov = line[6];
+        member.country = line[7];
+        member.mail_code = line[8];
+        member.phone_no = line[9];
+        member.issue_dt = line[10];
+        member.expr_dt = line[11];
+        member.corp_no = line[12];
+        member.prev_balance = line[13];
+        member.curr_balance = line[14];
+        member.member_code = line[15];
+
+        self.postMessage(member);
+
+    }   
+
+}
