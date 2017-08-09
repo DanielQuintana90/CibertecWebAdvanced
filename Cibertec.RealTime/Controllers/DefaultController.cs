6c800dea92aa51cf4d3d2e98631e30aee7ae8dde
NetWeb-MJ <NetWeb-MJ@S000CVI00>
2017-08-08 22:36:03 -0500

    SignalR

---
+++ Cibertec.RealTime/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Cibertec.RealTime.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < 3000; i++)
+            {
+                list.Add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            }
+
+            return Ok(list);
+        }
+    }
+}
---
+++ Cibertec.RealTime/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Cibertec.RealTime.App_Start;
+using Microsoft.Owin.Cors;
+using Owin;
+using System.Web.Http;
+
+namespace Cibertec.RealTime
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+                     
+            var httpConfig = new HttpConfiguration();
+            WebApiConfig.Register(httpConfig);
+
+            app.MapSignalR();
+
+            app.UseWebApi(httpConfig);            
+        }
+    }
+}
